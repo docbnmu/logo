66cea83c72199e1c54de6dc4c9a5404c16cc2553
compliment <nishkumar1@outlook.com>
2020-05-06 11:26:02 +0530

    Add all files.

---
+++ bnmu-logo_icons.dart
@@ -0,0 +1,16 @@
+// Place fonts/bnmu-logo.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: bnmu-logo
+//      fonts:
+//       - asset: fonts/bnmu-logo.ttf
+import 'package:flutter/widgets.dart';
+
+class Bnmu_logo {
+  Bnmu_logo._();
+
+  static const String _fontFamily = 'bnmu-logo';
+
+  static const IconData logo = IconData(0xe900, fontFamily: _fontFamily);
+}
